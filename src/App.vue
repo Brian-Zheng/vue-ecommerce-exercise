0618b80f1617fe0f87a85f17352f0cadc618c9ab
200067 <brianzheng@systex.com>
2019-08-19 22:44:25 +0800

    first commit  lesson 11.84

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  created () {
+    const api = `${process.env.API_PATH}/api/${process.env.CUSTOM_PATH}/products`;
+    this.$http.get(api).then(response => {
+      console.log(response.data);
+    });
+  }
+};
+</script>
+
+<style lang="scss">
+// #app {
+//   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+//   margin-top: 60px;
+// }
+
+@import "./assets/all";
+</style>
