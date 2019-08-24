ae0cbbbbe7fb157e6c375333bad2377a07c2d62a
200067 <brianzheng@systex.com>
2019-08-24 23:23:15 +0800

    11-92 新增、修改商品資料

--- src/components/pages/Products.vue
+++ src/components/pages/Products.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="text-right">
-      <button class="btn btn-primary mt-4" @click="openModal">建立新的產品</button>
+      <button class="btn btn-primary mt-4" @click="openModal(true)">建立新的產品</button>
     </div>
     <table class="table mt-4">
       <thead>
@@ -23,7 +23,7 @@
             <span v-else>未啟用</span>
           </td>
           <td>
-            <button class="btn btn-outline-primary btn-sm">編輯</button>
+            <button class="btn btn-outline-primary btn-sm" @click="openModal(false, item)">編輯</button>
           </td>
         </tr>
       </tbody>
@@ -37,18 +37,142 @@
       aria-labelledby="exampleModalLabel"
       aria-hidden="true"
     >
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content border-0">
+          <div class="modal-header bg-dark text-white">
+            <h5 class="modal-title" id="exampleModalLabel">
+              <span>新增產品</span>
+            </h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
-          <div class="modal-body">...</div>
+          <div class="modal-body">
+            <div class="row">
+              <div class="col-sm-4">
+                <div class="form-group">
+                  <label for="image">輸入圖片網址</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="image"
+                    v-model="tempProduct.imageUrl"
+                    placeholder="請輸入圖片連結"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="customFile">
+                    或 上傳圖片
+                    <i class="fas fa-spinner fa-spin" v-if="status.fileUploading"></i>
+                  </label>
+                  <input
+                    type="file"
+                    id="customFile"
+                    class="form-control"
+                    ref="files"
+                    @change="uploadFile"
+                  />
+                </div>
+                <img class="img-fluid" :src="tempProduct.imageUrl" alt />
+              </div>
+              <div class="col-sm-8">
+                <div class="form-group">
+                  <label for="title">標題</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="title"
+                    v-model="tempProduct.title"
+                    placeholder="請輸入標題"
+                  />
+                </div>
+
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <label for="category">分類</label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="category"
+                      v-model="tempProduct.category"
+                      placeholder="請輸入分類"
+                    />
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label for="price">單位</label>
+                    <input
+                      type="unit"
+                      class="form-control"
+                      id="unit"
+                      v-model="tempProduct.unit"
+                      placeholder="請輸入單位"
+                    />
+                  </div>
+                </div>
+
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <label for="origin_price">原價</label>
+                    <input
+                      type="number"
+                      class="form-control"
+                      id="origin_price"
+                      v-model="tempProduct.origin_price"
+                      placeholder="請輸入原價"
+                    />
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label for="price">售價</label>
+                    <input
+                      type="number"
+                      class="form-control"
+                      id="price"
+                      v-model="tempProduct.price"
+                      placeholder="請輸入售價"
+                    />
+                  </div>
+                </div>
+                <hr />
+
+                <div class="form-group">
+                  <label for="description">產品描述</label>
+                  <textarea
+                    type="text"
+                    class="form-control"
+                    id="description"
+                    v-model="tempProduct.description"
+                    placeholder="請輸入產品描述"
+                  ></textarea>
+                </div>
+                <div class="form-group">
+                  <label for="content">說明內容</label>
+                  <textarea
+                    type="text"
+                    class="form-control"
+                    id="content"
+                    v-model="tempProduct.content"
+                    placeholder="請輸入產品說明內容"
+                  ></textarea>
+                </div>
+                <div class="form-group">
+                  <div class="form-check">
+                    <input
+                      class="form-check-input"
+                      type="checkbox"
+                      v-model="tempProduct.is_enabled"
+                      :true-value="1"
+                      :false-value="0"
+                      id="is_enabled"
+                    />
+                    <label class="form-check-label" for="is_enabled">是否啟用</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
+            <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">取消</button>
+            <button type="button" class="btn btn-primary" @click="updateProduct">確認</button>
           </div>
         </div>
       </div>
@@ -57,12 +181,17 @@
 </template>
 
 <script>
-import $ from 'jquery';
+import $ from "jquery";
 
 export default {
   data() {
     return {
-      products: []
+      products: [],
+      tempProduct: {},
+      isNew: false,
+      status: {
+        fileUploading: false
+      }
     };
   },
   methods: {
@@ -77,8 +206,65 @@ export default {
         }
       });
     },
-    openModal() {
+    openModal(isNew, item) {
+      if (isNew) {
+        this.tempProduct = {};
+        this.isNew = true;
+      } else {
+        this.tempProduct = Object.assign({}, item);
+        this.isNew = false;
+      }
       $("#productModal").modal("show");
+    },
+    updateProduct() {
+      const vm = this;
+
+      let api = `${process.env.API_PATH}/api/${process.env.CUSTOM_PATH}/admin/product`;
+      let httpMethod = "post";
+      console.log(vm.isNew);
+      console.log(vm.tempProduct);
+      // vm.isNew若是false 表示為修改，需調整URL組成與Http Method
+      if (!vm.isNew) {
+        api = `${process.env.API_PATH}/api/${process.env.CUSTOM_PATH}/admin/product/${vm.tempProduct.id}`;
+        httpMethod = "put";
+      }
+      this.$http[httpMethod](api, { data: vm.tempProduct }).then(response => {
+        console.log(response.data);
+        if (response.data.success) {
+          $("#productModal").modal("hide");
+          vm.getProducts();
+        } else {
+          $("#productModal").modal("hide");
+          vm.getProducts();
+          console.log("新增商品失敗");
+        }
+      });
+    },
+    uploadFile() {
+      console.log(this);
+      const uploadedFile = this.$refs.files.files[0];
+      const vm = this;
+      const formData = new FormData();
+      formData.append("file-to-upload", uploadedFile);
+      const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/admin/upload`;
+      vm.status.fileUploading = true;
+      this.$http
+        .post(url, formData, {
+          headers: {
+            "Content-Type": "multipart/form-data"
+          }
+        })
+        .then(response => {
+          console.log(response.data);
+          vm.status.fileUploading = false;
+          if (response.data.success) {
+            // vm.tempProduct.imageUrl = response.data.imageUrl;
+            // console.log(vm.tempProduct);
+            vm.$set(vm.tempProduct, "imageUrl", response.data.imageUrl);
+          } else {
+            this.$bus.$emit("messsage:push", response.data.message, "danger");
+          }
+        });
     }
   },
   created() {
