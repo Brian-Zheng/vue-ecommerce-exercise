af28ad8cb09227f9b4b3177a26952450c573f762
200067 <brianzheng@systex.com>
2019-08-24 15:52:15 +0800

    11-88_update

--- src/main.js
+++ src/main.js
@@ -10,9 +10,34 @@ Vue.config.productionTip = false
 Vue.use(VueAxios, axios)
 
 /* eslint-disable no-new */
+axios.defaults.withCredentials = true;
+
 new Vue({
   el: '#app',
   router,
   components: { App },
   template: '<App/>'
-})
+});
+
+router.beforeEach((to, from, next) => {
+  console.log('to:', to);
+
+  if (to.meta.requiresAuth) {
+    // console.log('required auth');
+    const api = `${process.env.API_PATH}/api/user/check`;
+    // const vm = this;
+    axios.post(api).then((response) => {
+      console.log(response.data);
+      if (response.data.success) {
+        // vm.$router.push('/');
+        next();
+      } else {
+        next({
+          path: '/login'
+        });
+      }
+    })
+  } else {
+    next();
+  }
+});
