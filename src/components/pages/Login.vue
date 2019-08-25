383923e5b3a1d5a771707d91d909fdcd228b8db2
200067 <brianzheng@systex.com>
2019-08-25 13:34:03 +0800

    Login 頁面修復

--- src/components/pages/Login.vue
+++ src/components/pages/Login.vue
@@ -1,67 +1,107 @@
 <template>
-  <div class="message-alert">
-    <div class="alert alert-dismissible"
-      :class="'alert-' + item.status"
-      v-for="(item, i) in messages" :key="i">
-      {{ item.message }}
-      <button type="button" class="close" @click="removeMessage(i)" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
+  <div>
+    <form class="form-signin" @submit.prevent="signIn">
+      <h1 class="h3 mb-3 font-weight-normal">請先登入</h1>
+      <label for="inputUserName" class="sr-only">Email address</label>
+      <input
+        type="email"
+        id="inputUserName"
+        class="form-control"
+        placeholder="Email address"
+        required
+        autofocus
+        v-model="user.username"
+      />
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input
+        type="password"
+        id="inputPassword"
+        class="form-control"
+        placeholder="Password"
+        required
+        v-model="user.password"
+      />
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me" /> Remember me
+        </label>
+      </div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Navbar',
+  name: "Login",
   data() {
     return {
-      messages: []
+      user: {
+        username: "",
+        password: ""
+      }
     };
   },
   methods: {
-    updateMessage(message, status) {
-      const timestamp = Math.floor(new Date() / 1000);
-      this.messages.push({
-        message,
-        status,
-        timestamp
-      });
-      this.removeMessageWithTiming(timestamp);
-    },
-    removeMessage(num) {
-      this.messages.splice(num, 1);
-    },
-    removeMessageWithTiming(timestamp) {
+    signIn: function() {
+      const api = `${process.env.API_PATH}/admin/signin`;
       const vm = this;
-      setTimeout(() => {
-        vm.messages.forEach((item, i) => {
-          if (item.timestamp === timestamp) {
-            vm.messages.splice(i, 1);
-          }
-        });
-      }, 5000);
+      this.$http.post(api, vm.user).then(response => {
+        console.log(response.data);
+        if (response.data.success) {
+          vm.$router.push("/admin/products");
+        }
+      });
     }
-  },
-  created() {
-    const vm = this;
-    // 自定義名稱 'messsage:push'
-    // message: 傳入參數
-    // status: 樣式，預設值為 warning
-    vm.$bus.$on('messsage:push', (message, status = 'warning') => {
-      vm.updateMessage(message, status);
-    });
-    // vm.$bus.$emit('messsage:push');
   }
 };
 </script>
 
-<style scope>
-.message-alert {
-  position: fixed;
-  max-width: 50%;
-  top: 56px;
-  right: 20px;
-  z-index: 1100;
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+html,
+body {
+  height: 100%;
+}
+
+body {
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-align: center;
+  align-items: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #f5f5f5;
+}
+
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+}
+.form-signin .checkbox {
+  font-weight: 400;
+}
+.form-signin .form-control {
+  position: relative;
+  box-sizing: border-box;
+  height: auto;
+  padding: 10px;
+  font-size: 16px;
+}
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
 }
 </style>
